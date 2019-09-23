bd179ac2456180676948042c4c79113b5b8b3a6e
Neel Redkar <neel.redkar@outlook.com>
2019-09-23 15:49:38 -0700

    fix opaqueness and add FAQ & About popup

--- js/2020.js
+++ js/2020.js
@@ -1,15 +1,28 @@
 console.log("2020.js loaded");
 var aboutText = document.getElementById("text-about");
 var aboutCircle = document.getElementById("circle-about");
-var closeAbout = document.getElementById("close-about")
+var closeAbout = document.getElementById("close-about");
+var faqText = document.getElementById("text-faq");
+var faqCircle = document.getElementById("circle-faq");
+var closeFAQ = document.getElementById("close-faq");
 var shown = document.getElementById("shown")
 aboutCircle.onclick = ()=> {
     aboutText.style.visibility = "visible";
     aboutText.style.opacity = 1;
     shown.className="blur";
 };
-closeAbout.onclick = ()=> {
+closeAbout.onclick = () => {
     aboutText.style.visibility = "hidden";
     aboutText.style.opacity = 0;
-
+    shown.className="";
+}
+faqCircle.onclick = ()=> {
+    faqText.style.visibility = "visible";
+    faqText.style.opacity = 1;
+    shown.className="blur";
+};
+closeFAQ.onclick = () => {
+    faqText.style.visibility = "hidden";
+    faqText.style.opacity = 0;
+    shown.className="";
 }
